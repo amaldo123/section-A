edacddba00a8861c866dff9172db52e3f9f5a787
Amanuel <amanuelwassie39@gmail.com>
2025-03-23 15:38:02 +0300

    firstDay update

---
+++ test.py
@@ -0,0 +1,8 @@
+def StringToLowerCase(string):
+    print(string.lower())
+    
+def StringToUpperCase(string):
+    print(string.upper())
+    
+def StringToTitle(string):
+    print(string.title())
